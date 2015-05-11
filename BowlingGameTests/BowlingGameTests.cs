1da6aa25abdc41438903f5957a8c9fe3a10a5909
Arkadiusz Stępniewski <a.stepniewski@hotmail.com>
2015-05-11 22:35:59 +0200

    Test 2 red

--- BowlingGameTests/BowlingGameTests.cs
+++ BowlingGameTests/BowlingGameTests.cs
@@ -19,5 +19,18 @@ namespace BowlingGameTests
 
             Assert.AreEqual(0, game.Score);
         }
+
+        [TestMethod]
+        public void TestHittingOnePinPerRole()
+        {
+            var game = new Game();
+
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(1);
+            }
+
+            Assert.AreEqual(20, game.Score);
+        }
     }
 }
