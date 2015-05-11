a953da78d0bf70da7dbe4827e27a663d4337a7e0
Arkadiusz Stępniewski <arekstepek@gmail.com>
2015-05-11 22:11:15 +0200

    Commit solution

---
+++ BowlingGameTests/BowlingGameTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BowlingGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGameTests
+{
+    [TestClass]
+    public class BowlingGameTests
+    {
+        [TestMethod]
+        public void TestGutterGame()
+        {
+            var game = new Game();
+        }
+    }
+}
