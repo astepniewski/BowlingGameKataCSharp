201d65a90cf7b1f5aec6b92e4824c149a9172c59
Arkadiusz Stępniewski <a.stepniewski@hotmail.com>
2015-05-11 23:04:37 +0200

    Test 3 green

--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -8,16 +8,37 @@ namespace BowlingGame
 {
     public class Game
     {
-        public Game()
-        {
-            Score = 0;
-        }
-
-        public int Score { get; set; }
+        private List<int> _rolls = new List<int>(21);
+        private int currentRoll = 0;
 
         public void Roll(int pinsHit)
         {
-            Score += pinsHit;
+            _rolls.Add(pinsHit);
+            currentRoll++;
+        }
+
+        public int Score
+        {
+            get
+            {
+                int score = 0;
+                int i = 0;
+                for (int frame = 0; frame < 10; frame++)
+                {
+                    if (_rolls[i] + _rolls[i + 1] == 10)
+                    {
+                        //spare
+                        score += 10 + _rolls[i + 2];
+                        i += 2;
+                    }
+                    else
+                    {
+                        score += _rolls[i] + _rolls[i + 1];
+                        i += 2;
+                    }
+                }
+                return score;
+            }
         }
     }
 }
